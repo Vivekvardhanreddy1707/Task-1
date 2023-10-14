2867d5205ad64831875f903b30dedae8cd12ba84
Vivek Vardhan Reddy <vivekvardhanboreddy@gmail.com>
2023-10-14 09:32:52 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState } from "react";
 
 function App() {
+  const [text, setText] = useState("");
+
+  const handleInputChange = (event) => {
+    setText(event.target.value);
+  };
+
+  const countWords = (text) => {
+    let words = text.trim().split(/\s+/);
+    return words.length;
+  };
+
+  let wordCount = countWords(text); // Move this line inside the component
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="container">
+        <h1>Responsive Paragraph word counter</h1>
+        <textarea
+          value={text}
+          onChange={handleInputChange}
+          rows="4"
+          cols="50"
+          placeholder="Enter text here..."
+        />
+        <p>Word count: {wordCount}</p>
+      </div>
+    </>
   );
 }
 
